e4af2ff9178ed13eaaa42d68e4a3762a0ce4aff2
КЛИМ <JJJ>
2025-05-23 15:27:22 +0300

    kurit

---
+++ demkur/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace demkur
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
